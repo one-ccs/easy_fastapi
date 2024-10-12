1c3804b9b5863bbe434690518dcf4f3d06e91cb2
one-ccs <one-ccs@foxmail.com>
2024-10-12 17:18:15 +0800

    1、添加 model 描述 2、优化 user、role 关联逻辑

--- backend/app/models/role.py
+++ backend/app/models/role.py
@@ -9,6 +9,8 @@ from app.core import ExtendedCRUD
 class Role(ObjectUtil.MagicClass, ExtendedCRUD, Model):
     """角色表"""
 
-    id        = fields.IntField(primary_key=True)
-    role      = fields.CharField(max_length=16, db_index=True)
-    role_desc = fields.CharField(max_length=32)
+    id        = fields.IntField(primary_key=True, description='角色 id')
+    role      = fields.CharField(max_length=16, db_index=True, description='角色名称')
+    role_desc = fields.CharField(max_length=32, description='角色描述')
+
+    users: fields.ManyToManyRelation['User'] # type: ignore
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,22 +5,25 @@ from tortoise.expressions import Q
 from easy_pyoc import ObjectUtil
 
 from app.core import ExtendedCRUD
+from .role import Role
 
 
 class User(ObjectUtil.MagicClass, ExtendedCRUD, Model):
     """用户表"""
     _str_ignore = {'hashed_password'}
 
-    id              = fields.IntField(primary_key=True)
-    email           = fields.CharField(max_length=64, null=True, unique=True, db_index=True)
-    username        = fields.CharField(max_length=32, null=True, unique=True, db_index=True)
-    hashed_password = fields.CharField(max_length=64)
-    token           = fields.CharField(max_length=255, null=True)
-    avatar_url      = fields.CharField(max_length=256, null=True)
-    is_active       = fields.BooleanField(default=True)
-    created_at      = fields.DatetimeField(auto_now_add=True)
+    id              = fields.IntField(primary_key=True, description='用户 id')
+    email           = fields.CharField(max_length=64, null=True, unique=True, db_index=True, description='邮箱')
+    username        = fields.CharField(max_length=32, null=True, unique=True, db_index=True, description='用户名')
+    hashed_password = fields.CharField(max_length=64, description='密码')
+    token           = fields.CharField(max_length=256, null=True, description='访问令牌')
+    avatar_url      = fields.CharField(max_length=256, null=True, description='头像地址')
+    is_active       = fields.BooleanField(default=True, description='是否激活')
+    created_at      = fields.DatetimeField(auto_now_add=True, description='创建时间')
 
-    roles           = fields.ManyToManyField('models.Role', related_name='users')
+    roles: fields.ManyToManyRelation[Role] = fields.ManyToManyField(
+        'models.Role', related_name='users', through='user_role',
+    )
 
     @staticmethod
     async def by_username(username: str):
